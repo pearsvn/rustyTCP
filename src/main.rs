06f1c22dd51c692ef7aea88774044e4a0e190d42
pearsvn <alfie1107@hotmail.co.uk>
2026-08-28 03:10:06 +0100

    new interface + buffer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 // Date: 28.08.2026
 // Project: Implementing TCP in Rust
 
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf: [u8; 2] = [0, 255];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
